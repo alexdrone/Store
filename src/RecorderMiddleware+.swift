9b9e37145d168479102b37ed8078673e1333826c
Alex Usbergo <alexusbergo@alexusbergo-macpro.roam.corp.google.com>
2017-03-28 11:29:30 +0200

    Fix build issue

--- src/RecorderMiddleware+.swift
+++ src/RecorderMiddleware+.swift
@@ -1,4 +1,7 @@
+#if (arch(i386) || arch(x86_64)) && (os(iOS) || os(tvOS))
+
 import Foundation
+import UIKit
 
 final public class RecorderMiddleware: MiddlewareType {
 
@@ -103,116 +106,116 @@ final public class RecorderMiddleware: MiddlewareType {
 // MARK: - KeyCommands
 // forked from: Augustyniak/KeyCommands / Created by Rafal Augustyniak
 
-#if (arch(i386) || arch(x86_64)) && (os(iOS) || os(tvOS))
 
-  import UIKit
+struct KeyActionableCommand {
+  fileprivate let keyCommand: UIKeyCommand
+  fileprivate let actionBlock: () -> ()
 
-  struct KeyActionableCommand {
-    fileprivate let keyCommand: UIKeyCommand
-    fileprivate let actionBlock: () -> ()
+  func matches(_ input: String, modifierFlags: UIKeyModifierFlags) -> Bool {
+    return keyCommand.input == input && keyCommand.modifierFlags == modifierFlags
+  }
+}
 
-    func matches(_ input: String, modifierFlags: UIKeyModifierFlags) -> Bool {
-      return keyCommand.input == input && keyCommand.modifierFlags == modifierFlags
-    }
+func == (lhs: KeyActionableCommand, rhs: KeyActionableCommand) -> Bool {
+  return lhs.keyCommand.input == rhs.keyCommand.input
+    && lhs.keyCommand.modifierFlags == rhs.keyCommand.modifierFlags
+}
+
+public enum KeyCommands {
+  private static var __once: () = {
+    exchangeImplementations(class: UIApplication.self,
+                            originalSelector: #selector(getter: UIResponder.keyCommands),
+                            swizzledSelector: #selector(UIApplication.KYC_keyCommands));
+  }()
+  fileprivate struct Static {
+    static var token: Int = 0
   }
 
-  func == (lhs: KeyActionableCommand, rhs: KeyActionableCommand) -> Bool {
-    return lhs.keyCommand.input == rhs.keyCommand.input
-      && lhs.keyCommand.modifierFlags == rhs.keyCommand.modifierFlags
+  struct KeyCommandsRegister {
+    static var sharedInstance = KeyCommandsRegister()
+    fileprivate var actionableKeyCommands = [KeyActionableCommand]()
   }
 
-  public enum KeyCommands {
-    private static var __once: () = {
-      exchangeImplementations(class: UIApplication.self,
-                              originalSelector: #selector(getter: UIResponder.keyCommands),
-                              swizzledSelector: #selector(UIApplication.KYC_keyCommands));
-    }()
-    fileprivate struct Static {
-      static var token: Int = 0
+  /** Registers key command for specified input and modifier flags. Unregisters previously
+   *  registered key commands matching provided input and modifier flags. Does nothing when
+   * application runs on actual device.
+   */
+  public static func register(input: String,
+                              modifierFlags: UIKeyModifierFlags,
+                              action: @escaping () -> ()) {
+    _ = KeyCommands.__once
+    let keyCommand = UIKeyCommand(input: input,
+                                  modifierFlags: modifierFlags,
+                                  action: #selector(UIApplication.KYC_handleKeyCommand(_:)),
+                                  discoverabilityTitle: "")
+    let actionableKeyCommand = KeyActionableCommand(keyCommand: keyCommand, actionBlock: action)
+    let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
+      where: { return $0 == actionableKeyCommand })
+    if let index = index {
+      KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
     }
-
-    struct KeyCommandsRegister {
-      static var sharedInstance = KeyCommandsRegister()
-      fileprivate var actionableKeyCommands = [KeyActionableCommand]()
-    }
-
-    /** Registers key command for specified input and modifier flags. Unregisters previously
-     *  registered key commands matching provided input and modifier flags. Does nothing when
-     * application runs on actual device.
-     */
-    public static func register(input: String,
-                                modifierFlags: UIKeyModifierFlags,
-                                action: @escaping () -> ()) {
-      _ = KeyCommands.__once
-      let keyCommand = UIKeyCommand(input: input,
-                                    modifierFlags: modifierFlags,
-                                    action: #selector(UIApplication.KYC_handleKeyCommand(_:)),
-                                    discoverabilityTitle: "")
-      let actionableKeyCommand = KeyActionableCommand(keyCommand: keyCommand, actionBlock: action)
-      let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
-        where: { return $0 == actionableKeyCommand })
-      if let index = index {
-        KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
-      }
-      KeyCommandsRegister.sharedInstance.actionableKeyCommands.append(actionableKeyCommand)
-    }
-
-    /** Unregisters key command matching specified input and modifier flags.
-     *  Does nothing when application runs on actual device.
-     */
-    public static func unregister(input: String, modifierFlags: UIKeyModifierFlags) {
-      let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
-        where: { return $0.matches(input, modifierFlags: modifierFlags) })
-      if let index = index {
-        KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
-      }
-    }
+    KeyCommandsRegister.sharedInstance.actionableKeyCommands.append(actionableKeyCommand)
   }
 
-  extension UIApplication {
-    dynamic func KYC_keyCommands() -> [UIKeyCommand] {
-      return KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands.map({
-        return $0.keyCommand
-      })
+  /** Unregisters key command matching specified input and modifier flags.
+   *  Does nothing when application runs on actual device.
+   */
+  public static func unregister(input: String, modifierFlags: UIKeyModifierFlags) {
+    let index = KeyCommandsRegister.sharedInstance.actionableKeyCommands.index(
+      where: { return $0.matches(input, modifierFlags: modifierFlags) })
+    if let index = index {
+      KeyCommandsRegister.sharedInstance.actionableKeyCommands.remove(at: index)
     }
+  }
+}
 
-    func KYC_handleKeyCommand(_ keyCommand: UIKeyCommand) {
-      for command in KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands {
-        if command.matches(keyCommand.input, modifierFlags: keyCommand.modifierFlags) {
-          command.actionBlock()
-        }
+extension UIApplication {
+  dynamic func KYC_keyCommands() -> [UIKeyCommand] {
+    return KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands.map({
+      return $0.keyCommand
+    })
+  }
+
+  func KYC_handleKeyCommand(_ keyCommand: UIKeyCommand) {
+    for command in KeyCommands.KeyCommandsRegister.sharedInstance.actionableKeyCommands {
+      if command.matches(keyCommand.input, modifierFlags: keyCommand.modifierFlags) {
+        command.actionBlock()
       }
     }
   }
+}
 
-  func exchangeImplementations(class classs: AnyClass,
-                               originalSelector: Selector,
-                               swizzledSelector: Selector ){
-    let originalMethod = class_getInstanceMethod(classs, originalSelector)
-    let originalMethodImplementation = method_getImplementation(originalMethod)
-    let originalMethodTypeEncoding = method_getTypeEncoding(originalMethod)
-    let swizzledMethod = class_getInstanceMethod(classs, swizzledSelector)
-    let swizzledMethodImplementation = method_getImplementation(swizzledMethod)
-    let swizzledMethodTypeEncoding = method_getTypeEncoding(swizzledMethod)
-    let didAddMethod = class_addMethod(classs,
-                                       originalSelector,
-                                       swizzledMethodImplementation,
-                                       swizzledMethodTypeEncoding)
-    if didAddMethod {
-      class_replaceMethod(classs,
-                          swizzledSelector,
-                          originalMethodImplementation,
-                          originalMethodTypeEncoding)
-    } else {
-      method_exchangeImplementations(originalMethod, swizzledMethod)
-    }
+func exchangeImplementations(class classs: AnyClass,
+                             originalSelector: Selector,
+                             swizzledSelector: Selector ){
+  let originalMethod = class_getInstanceMethod(classs, originalSelector)
+  let originalMethodImplementation = method_getImplementation(originalMethod)
+  let originalMethodTypeEncoding = method_getTypeEncoding(originalMethod)
+  let swizzledMethod = class_getInstanceMethod(classs, swizzledSelector)
+  let swizzledMethodImplementation = method_getImplementation(swizzledMethod)
+  let swizzledMethodTypeEncoding = method_getTypeEncoding(swizzledMethod)
+  let didAddMethod = class_addMethod(classs,
+                                     originalSelector,
+                                     swizzledMethodImplementation,
+                                     swizzledMethodTypeEncoding)
+  if didAddMethod {
+    class_replaceMethod(classs,
+                        swizzledSelector,
+                        originalMethodImplementation,
+                        originalMethodTypeEncoding)
+  } else {
+    method_exchangeImplementations(originalMethod, swizzledMethod)
   }
+}
 
 #else
-  public enum KeyCommands {
-    public static func registerKeyCommand(input: String,
-                                          modifierFlags: UIKeyModifierFlags,
-                                          action: () -> ()) {}
-    public static func unregisterKeyCommand(input: String, modifierFlags: UIKeyModifierFlags) {}
-  }
+
+import Foundation
+
+public enum KeyCommands {
+  public static func registerKeyCommand(input: String,
+                                        modifierFlags: UIKeyModifierFlags,
+                                        action: () -> ()) {}
+  public static func unregisterKeyCommand(input: String, modifierFlags: UIKeyModifierFlags) {}
+}
 #endif
