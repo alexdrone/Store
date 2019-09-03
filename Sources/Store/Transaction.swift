be0952dd7dc7279f3bc2ba5d665a23e37e2598ec
Alex Usbergo <alexakdrone@gmail.com>
2019-09-03 10:17:05 +0200

    dependOn is now depend

--- Sources/Store/Transaction.swift
+++ Sources/Store/Transaction.swift
@@ -43,7 +43,7 @@ public protocol AnyTransaction: class {
 @available(iOS 13.0, macOS 10.15, *)
 public extension AnyTransaction {
   /// This transaction will execute after all of the operations in `transactions` are completed.
-  func dependOn(transactions: [AnyTransaction]) {
+  func depend(on transactions: [AnyTransaction]) {
     transactions.map { $0.operation }.forEach { operation.addDependency($0) }
   }
 }
@@ -59,55 +59,6 @@ public protocol ActionType: Identifiable {
   /// The execution body for this action.
   /// - note: Invoke `context.operation.finish` to signal task completion.
   func perform(context: TransactionContext<AssociatedStoreType, Self>)
-}
-
-// MARK: - Context
-
-@available(iOS 13.0, macOS 10.15, *)
-public struct TransactionContext<S: StoreType, A: ActionType> {
-  /// The operation that is currently running.
-  /// - note: Invoke `context.operation.finish` to signal task completion.
-  public let operation: AsyncOperation
-  /// The target store for this transaction.
-  public let store: S
-  /// Last recorded error (or side effects) in this dispatch group.
-  public let error: Dispatcher.TransactionGroupError
-  /// The current transaction.
-  public let transaction: Transaction<A>
-
-  /// Atomically update the store's model.
-  public func updateModel(closure: (inout S.ModelType) -> (Void)) {
-    store.updateModel(transaction: transaction, closure: closure)
-  }
-  
-  /// Terminates the operation if there was an error raised by a previous action in the following
-  /// transaction group.
-  public func rejectOnGroupError() -> Bool {
-    guard error.lastError != nil else {
-      return false
-    }
-    operation.finish()
-    return true
-  }
-  
-  /// Terminates this operation with an error.
-  public func reject(error: Error) {
-    self.error.lastError = error
-    operation.finish()
-  }
-
-  /// Terminates the operation.
-  public func fulfill() {
-    operation.finish()
-  }
-}
-
-@available(iOS 13.0, macOS 10.15, *)
-public extension ActionType {
-  /// Default identifier implementation.
-  var id: String {
-    return String(describing:type(of:self))
-  }
 }
 
 // MARK: - Implementation
