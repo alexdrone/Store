376de23bb86c46b6a4c366b5e9aa23ca81e39ce5
Alex Usbergo <alexakdrone@gmail.com>
2019-09-08 15:22:32 +0200

    log

--- Sources/Store/Log.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import os.log
-
-@available(iOS 13.0, macOS 10.15, *)
-public extension OSLog {
-  static let primary = OSLog(subsystem: "io.store.StoreService", category: "primary")
-  static let diff = OSLog(subsystem: "io.store.StoreService", category: "diff")
-}
--- Sources/Store/Middleware.swift
+++ Sources/Store/Middleware.swift
@@ -53,3 +53,12 @@ public final class LoggerMiddleware: MiddlewareType {
     return nanos
   }
 }
+
+// MARK: - Log Subsystems
+
+@available(iOS 13.0, macOS 10.15, *)
+public extension OSLog {
+  static let primary = OSLog(subsystem: "io.store.StoreService", category: "primary")
+  static let diff = OSLog(subsystem: "io.store.StoreService", category: "diff")
+}
+
